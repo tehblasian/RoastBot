4141437f0c2ffc9262cc9ac01c5e769c27b80817
tehblasian <tehblasian>
2017-04-15 21:53:46 -0400

    added app files

---
+++ RoastBot.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+from random import randint
+
+app = Flask(__name__)
+
+InsultList = ["You're a LOSER", 'Suck it', 'OP is a phaggot', 'come at me, bro', 'Please stop talking to me']
+
+
+@app.route("/", methods=['GET', 'POST'])
+def run():
+    reply = selectreply()
+    roast(reply)
+    return "<h1>The RoastBot is RUNNING</h1>"
+
+def roast(reply):
+    """Respond to incoming texts"""
+    response = MessagingResponse().message(reply)
+    return str(response)
+
+
+def selectreply():
+    random = randint(0, len(InsultList))
+    return InsultList[random]
